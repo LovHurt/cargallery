7b14f57eaf4511e10cf7ce14e4307dc57d09568e
lovhurt <alicandagidir@hotmail.com>
2025-01-24 00:49:53 +0300

    init cargallery

---
+++ src/test/java/com/lovehurts/cargallery/CargalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovehurts.cargallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
